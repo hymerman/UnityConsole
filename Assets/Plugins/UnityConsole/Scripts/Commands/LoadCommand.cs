3b93389bcc90638d06b9c94f3f6843ffca1a1825
Ben Hymers <b.hymers@gmail.com>
2016-11-08 13:33:45 +0000

    Add LoadSceneAdditive, UnloadScene, SetActiveScene and rename LOAD to LoadScene

--- Assets/Plugins/UnityConsole/Scripts/Commands/LoadCommand.cs
+++ Assets/Plugins/UnityConsole/Scripts/Commands/LoadCommand.cs
@@ -8,17 +8,17 @@ using UnityEngine.SceneManagement;
 
 namespace Wenzil.Console.Commands
 {
-    public static class LoadCommand
+    public static class LoadSceneCommand
     {
-        public static readonly string name = "LOAD";
+        public static readonly string name = "LoadScene";
         public static readonly string description = "Load the specified scene by name. Before you can load a scene you have to add it to the list of levels used in the game. Use File->Build Settings... in Unity and add the levels you need to the level list there.";
-        public static readonly string usage = "LOAD scene";
+        public static readonly string usage = "LoadScene scene";
 
         public static ConsoleCommandResult Execute(params string[] args)
         {
             if(args.Length == 0)
             {
-                return HelpCommand.Execute(LoadCommand.name);
+                return HelpCommand.Execute(LoadSceneCommand.name);
             }
             else
             {
@@ -43,6 +43,120 @@ namespace Wenzil.Console.Commands
                 return ConsoleCommandResult.Succeeded(string.Format("Success loading {0}.", sceneName));
             else
                 return ConsoleCommandResult.Failed(string.Format("Failed to load {0}. Are you sure it's in the list of levels in Build Settings?", sceneName));
+        }
+    }
+
+    public static class LoadSceneAdditiveCommand
+    {
+        public static readonly string name = "LoadSceneAdditive";
+        public static readonly string description = "Load the specified scene by name. Before you can load a scene you have to add it to the list of levels used in the game. Use File->Build Settings... in Unity and add the levels you need to the level list there.";
+        public static readonly string usage = "LoadSceneAdditive sceneName";
+
+        public static ConsoleCommandResult Execute( params string[] args )
+        {
+            if( args.Length == 0 )
+            {
+                return HelpCommand.Execute( LoadSceneAdditiveCommand.name );
+            }
+            else
+            {
+                return LoadSceneAdditive( args[ 0 ] );
+            }
+        }
+
+        private static ConsoleCommandResult LoadSceneAdditive( string sceneName )
+        {
+            try
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene( sceneName, LoadSceneMode.Additive );
+            }
+            catch
+            {
+                return ConsoleCommandResult.Failed( string.Format( "Failed to load {0}.", sceneName ) );
+            }
+
+            var scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName( sceneName );
+
+            if( scene.IsValid() && scene.isLoaded )
+                return ConsoleCommandResult.Succeeded( string.Format( "Success loading {0}.", sceneName ) );
+            else
+                return ConsoleCommandResult.Failed( string.Format( "Failed to load {0}. Are you sure it's in the list of levels in Build Settings?", sceneName ) );
+        }
+    }
+
+    public static class UnloadSceneCommand
+    {
+        public static readonly string name = "UnloadScene";
+        public static readonly string description = "Unloads the named scene";
+        public static readonly string usage = "UnloadScene sceneName";
+
+        public static ConsoleCommandResult Execute( params string[] args )
+        {
+            if( args.Length == 0 )
+            {
+                return HelpCommand.Execute( LoadSceneAdditiveCommand.name );
+            }
+            else
+            {
+                return UnloadScene( args[ 0 ] );
+            }
+        }
+
+        private static ConsoleCommandResult UnloadScene( string sceneName )
+        {
+            try
+            {
+                UnityEngine.SceneManagement.SceneManager.UnloadScene( sceneName );
+            }
+            catch
+            {
+                return ConsoleCommandResult.Failed( string.Format( "Failed to load {0}.", sceneName ) );
+            }
+
+            var scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName( sceneName );
+
+            if( scene.IsValid() && !scene.isLoaded )
+                return ConsoleCommandResult.Succeeded( string.Format( "Success unloading {0}.", sceneName ) );
+            else
+                return ConsoleCommandResult.Failed( string.Format( "Failed to unload {0}. Are you sure it's in the list of levels in Build Settings?", sceneName ) );
+        }
+    }
+
+    public static class SetActiveSceneCommand
+    {
+        public static readonly string name = "SetActiveScene";
+        public static readonly string description = "Sets the named scene as the active scene";
+        public static readonly string usage = "SetActiveScene sceneName";
+
+        public static ConsoleCommandResult Execute( params string[] args )
+        {
+            if( args.Length == 0 )
+            {
+                return HelpCommand.Execute( SetActiveSceneCommand.name );
+            }
+            else
+            {
+                return SetActiveScene( args[ 0 ] );
+            }
+        }
+
+        private static ConsoleCommandResult SetActiveScene( string sceneName )
+        {
+            var scene = UnityEngine.SceneManagement.SceneManager.GetSceneByName( sceneName );
+
+            try
+            {
+                UnityEngine.SceneManagement.SceneManager.SetActiveScene( scene );
+            }
+            catch
+            {
+                return ConsoleCommandResult.Failed( string.Format( "Failed to set {0} as active.", sceneName ) );
+            }
+
+            if( scene.IsValid() && UnityEngine.SceneManagement.SceneManager.GetActiveScene() == scene )
+                return ConsoleCommandResult.Succeeded( string.Format( "Success setting {0} as active.", sceneName ) );
+            else
+                return ConsoleCommandResult.Failed( string.Format( "Failed to set {0} as active. Are you sure it's in the list of levels in Build Settings?", sceneName ) );
         }
     }
 }
--- Assets/Plugins/UnityConsole/Scripts/DefaultCommands.cs
+++ Assets/Plugins/UnityConsole/Scripts/DefaultCommands.cs
@@ -9,7 +9,10 @@ namespace Wenzil.Console
         {
             ConsoleCommandsDatabase.RegisterCommand(QuitCommand.name, QuitCommand.description, QuitCommand.usage, QuitCommand.Execute);
             ConsoleCommandsDatabase.RegisterCommand(HelpCommand.name, HelpCommand.description, HelpCommand.usage, HelpCommand.Execute);
-            ConsoleCommandsDatabase.RegisterCommand(LoadCommand.name, LoadCommand.description, LoadCommand.usage, LoadCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(LoadSceneCommand.name, LoadSceneCommand.description, LoadSceneCommand.usage, LoadSceneCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(LoadSceneAdditiveCommand.name, LoadSceneAdditiveCommand.description, LoadSceneAdditiveCommand.usage, LoadSceneAdditiveCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(UnloadSceneCommand.name, UnloadSceneCommand.description, UnloadSceneCommand.usage, UnloadSceneCommand.Execute);
+            ConsoleCommandsDatabase.RegisterCommand(SetActiveSceneCommand.name, SetActiveSceneCommand.description, SetActiveSceneCommand.usage, SetActiveSceneCommand.Execute);
         }
     }
 }
